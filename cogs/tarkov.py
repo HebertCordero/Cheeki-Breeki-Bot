9d654d4685304c1800f7c9ebfaa0266fa43f6767
Hebert Cordero <36938694+HebertCordero@users.noreply.github.com>
2022-01-27 17:04:43 +0000

    Terraia commands added

--- cogs/tarkov.py
+++ cogs/tarkov.py
@@ -1,4 +1,8 @@
+import csv
+import random
 import discord
+import requests
+from tabulate import tabulate
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '!')
@@ -14,9 +18,103 @@ class Tarkov(commands.Cog):
     print('Bot is online')
   
   # Commands
-  @commands.command()
+  @commands.command(name='tarkov', help='Checks if tarkov commands are online! (ex: !tarkov) ')
   async def tarkov(self, ctx):
     await ctx.send('Tarkov commands online!')
+
+  # COMMAND: CREATE CUSTOM CHANNEL
+  @client.command(name='init', help='Creates a Cheeki Breeki Text-Channel! (Requires Admin permission) ')
+  @commands.has_permissions(administrator = True)
+  async def init(ctx):
+    guild = ctx.guild
+    await guild.create_text_channel(name='Cheeki Breeki')
+    embed = discord.Embed(title="New Text channel was created!", description="Name: **{}**".format('Cheeki Breeki'), color = 0xadd8e6, inline = False)
+    await ctx.channel.send(embed = embed)
+
+  # COMMAND: RANDOM SCAV LINE
+  @client.command(name='scav', help='Returns a random Scav line! (ex: !scav) ')
+  async def scav(self, ctx):
+    scav_line = ['Kefka!', 'prjamo bertsi', 'Cheeki breeki', 'Opachki', 'Vanon suka!', 'Paskuda!', 'Vot khuy!', 'Urod!', 'Davay mochi ikh!', 'Pidoras!', 'Khana te, ponyal!?']
+    embed = discord.Embed(title=random.choice(scav_line), description="-scav 2021-".format(), color = 0xadd8e6, inline = False)
+    await ctx.channel.send(embed = embed)
+
+  # COMMAND: Bullet Data
+  @client.command(name='bullet', help='Returns caliver info (ex: !bullet 5.56x45) ')
+  async def bullet(self, ctx, name):
+    b_dataRaw = './assets/bullet_data.csv'
+    with open(b_dataRaw, encoding="utf8") as f:
+        csv_reader = csv.reader(f)
+        temp_arr = []
+        for line in csv_reader:
+          if line[0] == name:
+            temp_arr.append(line)
+        print(tabulate(temp_arr, headers=['B.Type', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6','Dmg','Frag %','A.Type' ]))
+        embed = discord.Embed(title=name, description=tabulate(temp_arr, headers=['B.Type', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6','Dmg','Frag %','A.Type']).format(), color = 0xadd8e6, inline = False)
+        await ctx.channel.send(embed = embed)
+
+  @client.command(name='test', help='Returns a sample text (ex: !test TEXT RETURN) ')
+  async def test(self, ctx, name):
+    embed = discord.Embed(title=name, description=name.format(), color = 0xadd8e6, inline = False)
+    await ctx.channel.send(embed = embed)
+
+  # COMMAND: GET DAMAGE DATA
+  @client.command(name='item', help='Shows info of the item (ex: !item soap) ')
+  async def item(self, ctx, name):
+    #EXAMPLE: m855a1
+    new_query = """
+    {
+        itemsByName(name: "%s") {
+            id
+            name
+            shortName
+        }
+    }
+    """
+    new_query = new_query % (name)
+    response = requests.post('https://tarkov-tools.com/graphql', json={'query': new_query})
+    if response.status_code == 200:
+        #return response.json()
+        embed = discord.Embed(title="query", description=response.json(), color = 0xadd8e6,inline = False)
+        await ctx.channel.send(embed = embed)
+        print(response.json())
+    else:
+      print(response.status_code)
+      raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code))
+
+  # COMMAND: GET ITEM DATA
+  @client.command(name='price', help='Shows price of the item (ex: !price soap) ')
+  async def price(self, ctx, name):
+    #EXAMPLE: m855a1
+    new_query = """
+    {
+        itemsByName(name: "%s") {
+            name
+            shortName
+            basePrice
+            imageLink
+            traderPrices {
+              price
+              trader{
+                name
+              }
+            }
+        }
+    }
+    """
+    new_query = new_query % (name)
+    response = requests.post('https://tarkov-tools.com/graphql', json={'query': new_query})
+    if response.status_code == 200:
+        #return response.json()
+        #print(response.json()['data']['itemsByName'][0]['imageLink'])
+        imageURL = response.json()['data']['itemsByName'][0]['imageLink']
+        embed = discord.Embed(title=response.json()['data']['itemsByName'][0]['shortName'], description=response.json(), color = 0xadd8e6,inline = False)
+        embed.set_image(url=imageURL)
+        await ctx.channel.send(embed = embed)
+        print(response.json())
+    else:
+      print(response.status_code)
+      raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code))
+
 
 
 def setup(client):
--- cogs/terraria.py
+++ cogs/terraria.py
@@ -1,3 +1,6 @@
+import os
+import json
+import base64
 import discord
 import requests
 from discord.ext import commands
@@ -15,10 +18,71 @@ class Terraria(commands.Cog):
     print('Bot is online')
   
   # Commands
-  @commands.command()
+  @commands.command(name='terraria', help='Checks if terrara commands are online! (ex: !terraria) ')
   async def terraria(self, ctx):
     await ctx.send('Terraria commands online!')
+  
+  # Terraria Server Status
+  @client.command(name='t_status', help='Terraria Server Status (ex: !t_status) ')
+  async def t_status(self, ctx):
+    url = "https://46xbqzxllc.execute-api.us-east-1.amazonaws.com/default/terraria/status"
+    payload={}
+    headers = {
+      'x-api-key': os.getenv('terraria')
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    if response.status_code == 200:
+      resp = json.loads(response.text)
+      #return response.json()
+      #print(resp['body'])
+      embed = discord.Embed(title="Terraria server status:", description="current status: **{}**".format(resp['body']), color = 0xadd8e6, inline = False)
+      await ctx.channel.send(embed = embed)
+    else:
+      print(response.status_code)
+      embed = discord.Embed(title="Terraria server status:", description="current status: **{}**".format(response.status_code), color = 0xadd8e6, inline = False)
+      await ctx.channel.send(embed = embed)
 
+  # Terraria Server Status
+  @client.command(name='t_stop', help='Terraria Server STOP (ex: !t_stop) ')
+  async def t_stop(self, ctx):
+    url = "https://46xbqzxllc.execute-api.us-east-1.amazonaws.com/default/terraria/stop"
+    payload={}
+    headers = {
+      'x-api-key': os.getenv('terraria')
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    if response.status_code == 200:
+      resp = json.loads(response.text)
+      #return response.json()
+      #print(resp['body'])
+      embed = discord.Embed(title="Terraria server STOP:", description="current status: **{}**".format(resp['body']), color = 0xadd8e6, inline = False)
+      await ctx.channel.send(embed = embed)
+    else:
+      print(response.status_code)
+      embed = discord.Embed(title="Terraria server STOP:", description="current status: **{}**".format(response.status_code), color = 0xadd8e6, inline = False)
+      await ctx.channel.send(embed = embed)
+  
+  # Terraria Server Start
+  @client.command(name='t_start', help='Terraria Server START (ex: !t_start) ')
+  async def t_start(self, ctx):
+    url = "https://46xbqzxllc.execute-api.us-east-1.amazonaws.com/default/terraria/start"
+    payload={}
+    headers = {
+      'x-api-key': os.getenv('terraria')
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    if response.status_code == 200:
+      resp = json.loads(response.text)
+      #return response.json()
+      #print(resp['body'])
+      embed = discord.Embed(title="Terraria server START:", description="current status: **{}**".format(resp['body']), color = 0xadd8e6, inline = False)
+      await ctx.channel.send(embed = embed)
+    else:
+      print(response.status_code)
+      embed = discord.Embed(title="Terraria server START:", description="current status: **{}**".format(response.status_code), color = 0xadd8e6, inline = False)
+      await ctx.channel.send(embed = embed)
 
 def setup(client):
   client.add_cog(Terraria(client))
+
+#os.getenv('terraria') x-api-key
